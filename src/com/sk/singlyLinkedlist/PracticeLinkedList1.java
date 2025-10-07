f38b47d505af32df2ca771a1a1e71d11955572c9
shabbirkhan <khanshabbir126@gmail.com>
2025-10-07 19:19:05 +0530

    commit

---
+++ src/com/sk/singlyLinkedlist/PracticeLinkedList1.java
@@ -0,0 +1,18 @@
+package com.sk.singlyLinkedlist;
+
+public class PracticeLinkedList1 {
+
+	private ListNode head;
+	
+	public static class ListNode{
+		
+		private int data;
+		private ListNode next;
+		
+		public ListNode(int data)
+		{
+			this.data=data;
+			this.next=next;
+		}
+	}
+}
