dee55432867cb4b8e82d67061371a82209e561ce
shabbirkhan <khanshabbir126@gmail.com>
2025-10-20 00:04:43 +0530

    one set done

--- src/com/sk/DAY2.java
+++ src/com/sk/DAY2.java
@@ -8,12 +8,29 @@ public class DAY2 {
 		int n=arr.length;
 		for(int i=0;i< arr.length ;i++)
 		{
-			arr[i]=n-1-i;
-			System.out.print(arr[i]);
+			System.out.print(arr[arr.length - 1 -i] + " ");
+			
+		}
+		
+		int start=0;
+		int end=arr.length-1;
+		while(start < end)
+		{
+			int temp=arr[start];
+			arr[start]=arr[end];
+			arr[end]=temp;
+			start ++;
+			end --;
+		}
+		System.out.println("Next");
+		for(int i=0 ; i < arr.length;i++)
+		{
+			System.out.print(arr[i] +" ");
 		}
 	}
 	
 	public static void main(String[] args) {
-		
+		int [] arr= {1,3,4,7,8,9};
+		reverseArray(arr);
 	}
 }
