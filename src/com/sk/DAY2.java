805c503689ba51e52b66e19fc7c2c0cce3efa00a
shabbirkhan <khanshabbir126@gmail.com>
2025-10-19 23:50:26 +0530

    reverse practice done

--- src/com/sk/DAY2.java
+++ src/com/sk/DAY2.java
@@ -2,4 +2,18 @@ package com.sk;
 
 public class DAY2 {
 
+	//reverse Array..
+	public static void reverseArray(int[] arr)
+	{
+		int n=arr.length;
+		for(int i=0;i< arr.length ;i++)
+		{
+			arr[i]=n-1-i;
+			System.out.print(arr[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
 }
